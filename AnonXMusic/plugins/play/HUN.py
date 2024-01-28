b8ecc08928d4a78a703efa92c2ee1903dc641297
Coi SR <150426205+nossah56@users.noreply.github.com>
2024-01-28 11:49:20 +0200

    Update HUN.py

--- AnonXMusic/plugins/play/HUN.py
+++ AnonXMusic/plugins/play/HUN.py
@@ -3,7 +3,7 @@ import asyncio
 import os
 import time
 import requests
-from config import OWNER_ID, USER_OWNER
+from config import OWNER_ID
 from pyrogram import filters
 import random
 from pyrogram import Client
